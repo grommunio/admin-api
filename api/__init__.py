e17aaaced3180452599665f75398363ada96490b
Julia Schroeder <julia.schroeder@grammm.com>
2021-04-09 15:44:57 +0200

    Support loading of JSON OpenAPI spec
    The JSON loader has been tested to be significantly faster than its YAML
    counterpart. If `res/openapi.json` exists, it will be loaded, otherwise
    `res/openapi.yaml` will be used as fallback.

--- api/__init__.py
+++ api/__init__.py
@@ -2,19 +2,24 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020-2021 grammm GmbH
 
-import yaml
 
 BaseRoute = "/api/v1"  # Common prefix for all endpoints
 
 apiSpec = None  # API specification
 apiVersion = None  # API specification version. Extracted from the OpenAPI document.
-backendVersion = "0.19.5"  # Backend version number
+backendVersion = "0.19.6"  # Backend version number
 
 
 def _loadOpenApiSpec():
     global apiVersion, apiSpec
-    with open("res/openapi.yaml", "r") as file:
-        apiSpec = yaml.load(file, Loader=yaml.SafeLoader)
+    try:
+        import json
+        with open("res/openapi.json", "r") as file:
+            apiSpec = json.load(file)
+    except FileNotFoundError:
+        import yaml
+        with open("res/openapi.yaml", "r") as file:
+            apiSpec = yaml.load(file, Loader=yaml.SafeLoader)
     apiVersion = apiSpec["info"]["version"]
 
 
