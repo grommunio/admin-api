78afd3512d9c05a88deaf19dd350fe20ae54f07e
Julia Schroeder <julia.schroeder@grammm.com>
2021-05-17 10:49:36 +0200

    Log error responses

--- api/errors.py
+++ api/errors.py
@@ -25,3 +25,11 @@ def not_found(error):
 def method_not_allowed(error):
     """Return JSON object with 405 message."""
     return jsonify(message="Method '{}' not allowed on this endpoint".format(request.method)), 405
+
+@API.after_request
+def logError(response):
+    if response.status_code // 100 in (4, 5):
+        API.logger.warning("{} {} from {} -> {} {}".format(request.method, request.full_path, request.host,
+                                                             response.status_code, repr(str(response.data, "utf-8"))
+                                                             if response.is_json else "<data>"))
+    return response
