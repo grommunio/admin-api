grommunio/admin-api

commit f73d862146525854e741bdf36a33e1f3750203b8

Implement user search by property

api/__init__.py
@@ -7,7 +7,7 @@ BaseRoute = "/api/v1"  # Common prefix for all endpoints
 
 apiSpec = None  # API specification
 apiVersion = None  # API specification version. Extracted from the OpenAPI document.
-backendVersion = "1.11.10"  # Backend version number
+backendVersion = "1.11.11"  # Backend version number
 
 
 def _loadOpenApiSpec():

endpoints/__init__.py
@@ -339,3 +339,70 @@ def defaultObjectHandler(Model, ID, name, filters=()):
         return defaultPatch(Model, ID, name, None, filters)
     elif request.method == "DELETE":
         return defaultDelete(Model, ID, name, filters)
+
+
+def userQuery(domainID=None):
+    """Query users.
+
+    Parameters
+    ----------
+    domainID : int, optional
+        Filter by domain ID. The default is None.
+
+    Returns
+    -------
+    Response
+        JSON response containing user data
+    """
+    from sqlalchemy import or_, String
+    from sqlalchemy.orm import aliased
+    from orm.users import Users, UserProperties
+    from tools.constants import PropTags
+    from tools.DataModel import DataModel
+    from tools.misc import createMapping
+
+    Users._init()
+    verbosity = int(request.args.get("level", 1))
+    query, limit, offset, _ = defaultListHandler(Users, filters=(Users.domainID == domainID,) if domainID is not None else (),
+                                                 result="query", include_count=None, automatch=False)
+    sorts = request.args.getlist("sort")
+    for s in sorts:
+        sprop, sorder = s.split(",", 1) if "," in s else (s, "asc")
+        if hasattr(PropTags, sprop.upper()):
+            up = aliased(UserProperties)
+            query = query.join(up, (up.userID == Users.ID) & (up.tag == getattr(PropTags, sprop.upper())))\
+                         .order_by(up._propvalstr.desc() if sorder == "desc" else up._propvalstr.asc())
+
+    if "match" in request.args:
+        expr = request.args["match"]
+        fields = set(request.args["matchFields"].split(",")) if "matchFields" in request.args else None
+        isUnicode = any(ord(c) > 127 for c in expr)
+        matchexpr = tuple("%"+substr+"%" for substr in expr.split())
+        matchables = Users._meta.matchables if fields is None else (m for m in Users._meta.matchables if m.alias in fields)
+        targets = []
+        for prop in matchables:
+            column, query = prop.resolve(Users, query)
+            if not (isUnicode and isinstance(column.type, String) and column.type.charset == "ascii"):
+                targets.append((prop, column))
+        if "matchProps" in request.args:
+            metaProp = DataModel.Prop(None)
+            for prop in request.args["matchProps"].split(","):
+                if hasattr(PropTags, prop.upper()):
+                    up = aliased(UserProperties)
+                    query = query.outerjoin(up, (up.userID == Users.ID) & (up.tag == getattr(PropTags, prop.upper())))
+                    targets.append((metaProp, up._propvalstr))
+        filters = [column.ilike(match) for match in matchexpr for prop, column in targets if prop.match == "default"] +\
+                  [column == prop.tf(expr) for prop, column in targets if prop.match == "exact" and prop.tf(expr) is not None]
+        query = query.filter(or_(filter for filter in filters) if filters else False).reset_joinpoint()
+
+    count = query.count()
+    data = [user.todict(verbosity) for user in query.limit(limit).offset(offset).all()]
+    if verbosity < 2 and "properties" in request.args:
+        tags = [getattr(PropTags, prop.upper(), None) for prop in request.args["properties"].split(",")]
+        for user in data:
+            user["properties"] = {}
+        usermap = createMapping(data, lambda x: x["ID"])
+        properties = UserProperties.query.filter(UserProperties.userID.in_(usermap.keys()), UserProperties.tag.in_(tags)).all()
+        for prop in properties:
+            usermap[prop.userID]["properties"][prop.name] = prop.val
+    return jsonify(count=count, data=data)

endpoints/domain/users.py
@@ -10,16 +10,15 @@ from base64 import b64decode
 from datetime import datetime
 from flask import request, jsonify
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import aliased
 
-from .. import defaultListHandler, defaultObjectHandler
+from .. import defaultObjectHandler, userQuery
 
 from services import Service
 
 from tools import formats
 from tools.config import Config
 from tools.constants import PropTags, PropTypes, ExchangeErrors, PrivateFIDs, Permissions
-from tools.misc import createMapping, loadPSO, GenericObject
+from tools.misc import loadPSO, GenericObject
 from tools.permissions import SystemAdminPermission, DomainAdminPermission, DomainAdminROPermission
 from tools.rop import nxTime, makeEidEx
 from tools.storage import setDirectoryOwner, setDirectoryPermission
@@ -37,26 +36,7 @@ from orm import DB
 @secure(requireDB=True)
 def getUsers(domainID):
     checkPermissions(DomainAdminROPermission(domainID))
-    from orm.users import Users, UserProperties
-    verbosity = int(request.args.get("level", 1))
-    query, limit, offset, count = defaultListHandler(Users, filters=(Users.domainID == domainID,), result="query")
-    sorts = request.args.getlist("sort")
-    for s in sorts:
-        sprop, sorder = s.split(",", 1) if "," in s else (s, "asc")
-        if hasattr(PropTags, sprop.upper()):
-            up = aliased(UserProperties)
-            query = query.join(up, (up.userID == Users.ID) & (up.tag == getattr(PropTags, sprop.upper())))\
-                         .order_by(up._propvalstr.desc() if sorder == "desc" else up._propvalstr.asc())
-    data = [user.todict(verbosity) for user in query.limit(limit).offset(offset).all()]
-    if verbosity < 2 and "properties" in request.args:
-        tags = [getattr(PropTags, prop.upper(), None) for prop in request.args["properties"].split(",")]
-        for user in data:
-            user["properties"] = {}
-        usermap = createMapping(data, lambda x: x["ID"])
-        properties = UserProperties.query.filter(UserProperties.userID.in_(usermap.keys()), UserProperties.tag.in_(tags)).all()
-        for prop in properties:
-            usermap[prop.userID]["properties"][prop.name] = prop.val
-    return jsonify(count=count, data=data)
+    return userQuery(domainID)
 
 
 @API.route(api.BaseRoute+"/domains/<int:domainID>/users", methods=["POST"])

endpoints/system/roles.py
@@ -3,42 +3,20 @@
 # SPDX-FileCopyrightText: 2020 grommunio GmbH
 
 from flask import jsonify, request
-from sqlalchemy.orm import aliased
 
 import api
 from api.core import API, secure
 from api.security import checkPermissions
 
-from tools.constants import PropTags
-from tools.misc import createMapping
 from tools.permissions import Permissions, SystemAdminPermission, SystemAdminROPermission
-from .. import defaultListHandler, defaultObjectHandler
+from .. import defaultListHandler, defaultObjectHandler, userQuery
 
 
 @API.route(api.BaseRoute+"/system/users", methods=["GET"])
 @secure(requireDB=True)
 def userListEndpointUnrestricted():
     checkPermissions(SystemAdminROPermission())
-    from orm.users import Users, UserProperties
-    verbosity = int(request.args.get("level", 1))
-    query, limit, offset, count = defaultListHandler(Users, (Users.ID != 0,), result="query")
-    sorts = request.args.getlist("sort")
-    for s in sorts:
-        sprop, sorder = s.split(",", 1) if "," in s else (s, "asc")
-        if hasattr(PropTags, sprop.upper()):
-            up = aliased(UserProperties)
-            query = query.join(up, (up.userID == Users.ID) & (up.tag == getattr(PropTags, sprop.upper())))\
-                         .order_by(up._propvalstr.desc() if sorder == "desc" else up._propvalstr.asc())
-    data = [user.todict(verbosity) for user in query.limit(limit).offset(offset).all()]
-    if verbosity < 2 and "properties" in request.args:
-        tags = [getattr(PropTags, prop.upper(), None) for prop in request.args["properties"].split(",")]
-        for user in data:
-            user["properties"] = {}
-        usermap = createMapping(data, lambda x: x["ID"])
-        properties = UserProperties.query.filter(UserProperties.userID.in_(usermap.keys()), UserProperties.tag.in_(tags)).all()
-        for prop in properties:
-            usermap[prop.userID]["properties"][prop.name] = prop.val
-    return jsonify(count=count, data=data)
+    return userQuery()
 
 
 @API.route(api.BaseRoute+"/system/roles/permissions", methods=["GET"])

res/openapi.yaml
@@ -2,7 +2,7 @@ openapi: 3.0.0
 info:
   title: grommunio Admin API
   description: grommunio administration REST API
-  version: 1.11.2
+  version: 1.11.3
 servers:
   - url: '/api/v1'
     description: Default deployment path
@@ -957,6 +957,7 @@ paths:
         - $ref: '#/components/parameters/queryOffset'
         - $ref: '#/components/parameters/match'
         - $ref: '#/components/parameters/matchFields'
+        - $ref: '#/components/parameters/matchProps'
         - $ref: '#/components/parameters/propnames'
       responses:
         '200':
@@ -1746,6 +1747,7 @@ paths:
         - $ref: '#/components/parameters/queryOffset'
         - $ref: '#/components/parameters/match'
         - $ref: '#/components/parameters/matchFields'
+        - $ref: '#/components/parameters/matchProps'
         - name: sort
           description: Sort by attribute. Can be given multiple times.
           in: query
@@ -4538,6 +4540,12 @@ components:
     matchFields:
       name: matchFields
       description: Comma separated list of attributes to restrict matching to.
+      in: query
+      schema:
+        type: string
+    matchProps:
+      name: matchProps
+      description: Comma separated list of user properties to match
       in: query
       schema:
         type: string

commit 4a0603d268323512889eafe999d172664533c52c

Remove admin from user list
Fix regression caused by f73d862146525854e741bdf36a33e1f3750203b8.

api/__init__.py
@@ -7,7 +7,7 @@ BaseRoute = "/api/v1"  # Common prefix for all endpoints
 
 apiSpec = None  # API specification
 apiVersion = None  # API specification version. Extracted from the OpenAPI document.
-backendVersion = "1.11.12"  # Backend version number
+backendVersion = "1.11.13"  # Backend version number
 
 
 def _loadOpenApiSpec():

endpoints/__init__.py
@@ -363,8 +363,9 @@ def userQuery(domainID=None):
 
     Users._init()
     verbosity = int(request.args.get("level", 1))
-    query, limit, offset, _ = defaultListHandler(Users, filters=(Users.domainID == domainID,) if domainID is not None else (),
-                                                 result="query", include_count=None, automatch=False)
+    filters = (Users.domainID == domainID,) if domainID is not None else ()
+    filters += (Users.ID > 0,)
+    query, limit, offset, _ = defaultListHandler(Users, filters=filters, result="query", include_count=None, automatch=False)
     sorts = request.args.getlist("sort")
     for s in sorts:
         sprop, sorder = s.split(",", 1) if "," in s else (s, "asc")