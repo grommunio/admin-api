919471018ae613c5caab4b65ca82db9524272ad8
Christian <christian.siegrist@open-circle.ch>
2026-08-19 14:47:30 +0200

    cli: Fix user create/modify crash without --alias
    Signed-off-by: Christian <christian.siegrist@open-circle.ch>

--- cli/user.py
+++ cli/user.py
@@ -172,7 +172,7 @@ def _splitData(args):
     if "storeprop" in attributes or "remove_storeprop" in attributes:
         cli.print(cli.col("--storeprop and --remove_storeprop arguments are deprecated, use --property and --remove_property "
                           "instead.", "yellow"))
-    data["aliases"] = [alias for alias in attributes.pop("alias", None) if alias.strip()] or ()
+    data["aliases"] = [alias for alias in attributes.pop("alias", None) or [] if alias.strip()] or ()
     data["aliases_rm"] = attributes.pop("remove_alias", None) or ()
     data["altnames"] = [{"altname": altname} for altname in attributes.pop("altname", None) or [] if altname.strip()]
     data["altnames_rm"] = attributes.pop("remove_altname", None) or {}
